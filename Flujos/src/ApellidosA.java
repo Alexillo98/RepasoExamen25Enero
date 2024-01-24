0c5962dfb2c5c9e38bfb88123e28a67cea07db8e
Alexillo98 <musateduardalexandru@gmail.com>
2024-01-24 11:30:52 +0100

    Commmit

--- Flujos/src/ApellidosA.java
+++ Flujos/src/ApellidosA.java
@@ -5,5 +5,19 @@ public class ApellidosA {
     public static void main(String[] args) {
         List<Person> personas = new ArrayList<>();
 
+        Person persona = new Person("Lana","Alvarez",1998);
+        personas.add(persona);
+
+        persona = new Person("Marco","Polo",1956);
+        personas.add(persona);
+
+        persona = new Person("Alex","Musat",1998);
+        personas.add(persona);
+
+        long porA = personas.stream()
+                .filter(person -> person.getLastName().startsWith("A"))
+                .count();
+
+        System.out.println(porA);
     }
 }
